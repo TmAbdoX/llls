8136f12926c4bfe8a903382e2a24cb0ce051ee87
MD-MXX <103865983+MD-MXX@users.noreply.github.com>
2022-04-21 03:56:06 +0200

    Update dev.py

--- YukkiMusic/plugins/play/dev.py
+++ YukkiMusic/plugins/play/dev.py
@@ -40,7 +40,7 @@ async def khalid(client: Client, message: Message):
         photo=f"{IMG_DEV1}",
        caption=f"""❲ Developers Bot ❳
 — — — — — — — — —
-User: {OWNER_user.mention}
+𖥔Dev Name :  [{OWNER_NAME}](https://t.me/{OWNER})
 𖥔 Dev User : @{OWNER}""",
         reply_markup=InlineKeyboardMarkup(
             [
