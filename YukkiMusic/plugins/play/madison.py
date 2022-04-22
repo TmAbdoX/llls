13c7c473d4a8cb6975e646b5ccbbdb17c0cbb59e
MD-MXX <103865983+MD-MXX@users.noreply.github.com>
2022-04-22 05:30:12 +0200

    Create madison.py

---
+++ YukkiMusic/plugins/play/madison.py
@@ -0,0 +1,26 @@
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+
+@app.on_message(
+     command(["مبرمج السورس","ماديسون","احمد ماديسون"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/02ff5eed4dc2a34cbc1f7.jpg",
+        caption=f"""- مبرمج سورس مكس .""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "◜ 𝙈َ𝙖 𝘿ِ𝙞 𝙎ُ𝙤𝙉 ◞", url=f"https://t.me/MaDyY_y"),
+                ],
+            ]
+        ),
+    )
